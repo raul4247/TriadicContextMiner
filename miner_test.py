5c21578f78364e462a794afa1211e1ca840530ba
Raul <raulfmansur@hotmail.com>
2020-08-16 18:53:44 -0300

    Fix on test order

--- miner_test.py
+++ miner_test.py
@@ -9,90 +9,84 @@ from TriadicContext import TriadicContext
 class MyTestCase(unittest.TestCase):
 
     @classmethod
-    def setUpClass(self):
+    def setUpClass(cls):
         triadic_context = TriadicContext.read_triadic_context(configs.input_file)
 
         context, objects_count, attributes_count = TriadicContext.to_dyadic(triadic_context)
-        self.dyadic_context = DyadicContext(context, objects_count, attributes_count)
-        self.dyadic_context.save(configs.dyadic_file)
+        cls.dyadic_context = DyadicContext(context, objects_count, attributes_count)
+        cls.dyadic_context.save(configs.dyadic_file)
 
         # dyadic_context.mine_concepts(configs.dyadic_file, configs.concepts_file)
-        self.dyadic_context.read_concepts_from_file(configs.concepts_file)
+        cls.dyadic_context.read_concepts_from_file(configs.concepts_file)
 
-        self.dyadic_context.iPred()
-        self.dyadic_context.save_links(configs.links_file)
+        cls.dyadic_context.iPred()
+        cls.dyadic_context.save_links(configs.links_file)
 
-        self.dyadic_context.compute_feature_generators()
+        cls.dyadic_context.compute_feature_generators()
 
     def test_generators(self):
         self.assertEqual(Counter(
-            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '3', '5', '2', '1'})]]),
-            Counter(
-                [frozenset({'Ra'}), frozenset({'Nd'}), frozenset({'Pd'}), frozenset({'Ka'}), frozenset({'Pa'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '2', '3', '5'})]]),
-            Counter(
-                [frozenset({'Rb'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '1', '3', '5'})]]),
-            Counter(
-                [frozenset({'Kb'})]))
+            [frozenset({'Ra'}), frozenset({'Nd'}), frozenset({'Pd'}), frozenset({'Ka'}), frozenset({'Pa'})]),
+            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '3', '5', '2', '1'})]]))
+
+        self.assertEqual(Counter([frozenset({'Rb'})]), Counter(
+            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '2', '3', '5'})]]))
+
+        self.assertEqual(Counter([frozenset({'Kb'})]), Counter(
+            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '1', '3', '5'})]]))
+
+        self.assertEqual(Counter([frozenset({'Rb', 'Kb'})]), Counter(
+            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '3', '5'})]]))
+
+        self.assertEqual(Counter([frozenset({'Pb'})]), Counter(
+            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '1', '3'})]]))
+
+        self.assertEqual(Counter([frozenset({'Pb', 'Rb'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '3'})]]))
+
+        self.assertEqual(Counter([frozenset({'Sa'})]), Counter(
+            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '3', '5'})]]))
+
+        self.assertEqual(Counter([frozenset({'Rb', 'Sa'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'3', '5'})]]))
+
+        self.assertEqual(Counter([frozenset({'Pb', 'Sa'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '3'})]]))
+
+        self.assertEqual(Counter([frozenset({'Pb', 'Rb', 'Sa'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'3'})]]))
+
+        self.assertEqual(Counter([frozenset({'Nb'})]), Counter(
+            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '2', '1'})]]))
+
+        self.assertEqual(Counter([frozenset({'Sd'}), frozenset({'Nb', 'Rb'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '2'})]]))
+
+        self.assertEqual(Counter([frozenset({'Kb', 'Nb'}), frozenset({'Nb', 'Pb'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '4'})]]))
+
+        self.assertEqual(Counter([frozenset({'Pb', 'Sd'}), frozenset({'Sd', 'Kb'}), frozenset({'Pb', 'Nb', 'Rb'}),
+                                  frozenset({'Kb', 'Nb', 'Rb'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4'})]]))
+
+        self.assertEqual(Counter([frozenset({'Rd'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'2', '5'})]]))
+
+        self.assertEqual(Counter([frozenset({'Na'})]), Counter(
+            self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '5'})]]))
+
+        self.assertEqual(Counter(
+            [frozenset({'Kc'}), frozenset({'Rb', 'Na'}), frozenset({'Sa', 'Rd'}), frozenset({'Na', 'Rd'}),
+             frozenset({'Kb', 'Rd'})]),
+                         Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'5'})]]))
 
         self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '3', '5'})]]), Counter(
-                [frozenset({'Rb', 'Kb'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '1', '3'})]]), Counter(
-                [frozenset({'Pb'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '3'})]]), Counter(
-                [frozenset({'Pb', 'Rb'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '3', '5'})]]), Counter(
-                [frozenset({'Sa'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'3', '5'})]]), Counter(
-                [frozenset({'Rb', 'Sa'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '3'})]]), Counter(
-                [frozenset({'Pb', 'Sa'})]))
-        self.assertEqual(Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'3'})]]),
-                         Counter(
-                             [frozenset({'Pb', 'Rb', 'Sa'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '2', '1'})]]), Counter(
-                [frozenset({'Nb'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4', '2'})]]), Counter(
-                [frozenset({'Sd'}), frozenset({'Nb', 'Rb'})]))
+            Counter([frozenset({'Kd'}), frozenset({'Nc'}), frozenset({'Sd', 'Rd'}), frozenset({'Rd', 'Nb'})]),
+            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'2'})]]))
 
         self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '4'})]]), Counter(
-                [frozenset({'Kb', 'Nb'}), frozenset({'Nb', 'Pb'})]))
+            Counter([frozenset({'Rc'}), frozenset({'Pb', 'Na'}), frozenset({'Nb', 'Sa'}), frozenset({'Nb', 'Na'})]),
+            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1'})]]), )
 
-        self.assertEqual(Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'4'})]]),
-                         Counter(
-                             [frozenset({'Pb', 'Sd'}), frozenset({'Sd', 'Kb'}), frozenset({'Pb', 'Nb', 'Rb'}),
-                              frozenset({'Kb', 'Nb', 'Rb'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'2', '5'})]]), Counter(
-                [frozenset({'Rd'})]))
-        self.assertEqual(
-            Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1', '5'})]]), Counter(
-                [frozenset({'Na'})]))
-        self.assertEqual(Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'5'})]]),
-                         Counter(
-                             [frozenset({'Kc'}), frozenset({'Rb', 'Na'}), frozenset({'Sa', 'Rd'}),
-                              frozenset({'Na', 'Rd'}),
-                              frozenset({'Kb', 'Rd'})]))
-        self.assertEqual(Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'2'})]]),
-                         Counter(
-                             [frozenset({'Kd'}), frozenset({'Nc'}), frozenset({'Sd', 'Rd'}), frozenset({'Rd', 'Nb'})]))
-        self.assertEqual(Counter(self.dyadic_context.generators[self.dyadic_context.concepts[frozenset({'1'})]]),
-                         Counter(
-                             [frozenset({'Rc'}), frozenset({'Pb', 'Na'}), frozenset({'Nb', 'Sa'}),
-                              frozenset({'Nb', 'Na'})]))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
